5a0a83b5f2399197ad06d16bc16c79acc68994fa
Eli <eliperez0024@gmail.com>
2026-01-31 16:36:25 -0800

    fixed current word not clearing properly

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,7 @@ use std::{collections::BTreeMap, io::Read};
 
 
 fn main() {
-    println!("Hello, world!");
-
+    // Read file name from program args
     let args = std::env::args().collect::<Vec<String>>();
     let file_name = match args.get(1) {
         Some(file_name) => file_name,
@@ -13,6 +12,11 @@ fn main() {
         },
     };
 
+    if args.len() > 2 {
+        eprintln!("Program only takes one argument: File name");
+        return;
+    }
+
     let file = match std::fs::File::open(file_name) {
         Ok(file) => file,
         Err(e) => {
@@ -21,37 +25,44 @@ fn main() {
         },
     };
 
+    // Using btree to automatically sort lexographiclly as we insert new keys
     let mut map: BTreeMap<String, Vec<usize>>  = BTreeMap::new();
-    let mut index = 0;
+    let mut current_char = 0;
 
+    // Using buf reader to limit memory usage & use less sys calls
     let reader = std::io::BufReader::new(file);
     let mut current_word = String::new();
 
+    
     for byte in reader.bytes() {
         match byte {
             Ok(byte) => {
-                // Read byte as char
                 match char::from_u32(byte as u32) {
-                    Some(char) => match char.is_whitespace() {
-                        // If current byte is whitespace, add to the map.
-                        true => {
+                    Some(char) => {
+                        if char.is_whitespace() {
+                            // If btree map contains key, append index to indices list
                             if map.contains_key(&current_word) {
                                 let mapped_word = map.get_mut(&current_word);
                                 match mapped_word {
-                                    Some(mapped_word) => mapped_word.push(index - current_word.len()),
+                                    Some(mapped_word) => {
+                                        mapped_word.push(current_char - current_word.len());
+                                        current_word.clear();
+                                    },
                                     None => unreachable!(),
                                 }
-                            } else {
-                                map.insert(current_word.clone(), vec![index - current_word.len()]);
+                            } else if !current_word.is_empty() {
+                                map.insert(current_word.clone(), vec![current_char - current_word.len()]);
                                 current_word.clear();
                             }
-                            
-                        },
+                        }
                         // Else push the char onto the current word buffer
-                        false => current_word.push(char),
+                        else {
+                            current_word.push(char)
+                        }
                     },
                     None => {
                         eprintln!("Failed to convert byte {} into char.", byte);
+                        return;
                     },
                 }
             },
@@ -61,9 +72,31 @@ fn main() {
             },
         };
 
-        index += 1;
+        // Increment current byte
+        current_char += 1;
     }
 
-    dbg!(map);
+    // Handle if last word is not white space
+    if !current_word.is_empty() {
+        if map.contains_key(&current_word) {
+            let mapped_word = map.get_mut(&current_word);
+            match mapped_word {
+                Some(mapped_word) => mapped_word.push(current_char - current_word.len()),
+                None => unreachable!(),
+            }
+        } else if !current_word.is_empty() {
+            map.insert(current_word.clone(), vec![current_char - current_word.len()]);
+            current_word.clear();
+        }
+    }
+
+    // Output
+    for (key, indices) in map.iter() {
+        print!("{key} ");
+        for indice in indices {
+            print!("{indice} ");
+        }
+        println!();
+    }
 
 }
